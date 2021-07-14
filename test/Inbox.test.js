6c6924b3dc9908532cc7a7b6bef6b9963f8dc1f8
Oladokun Oladapo <oladokun104@gmail.com>
2021-07-14 08:12:12 +0100

    get and set message tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,41 +2,49 @@
 const assert = require('assert')
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const {interface, bytecode} = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 
+let accounts;
+let inbox;
+const HI_THERE = "Hi there!"
+const BYE = "bye"
 
-class Car{
-    park(){
-        return 'stopped';
-    }
-
-
-    drive(){
-        return 'vroom';
-    }
-}
-
-
-let car;
-
-beforeEach(() =>{
-    car = new Car();
-})
-
-
-describe('Car', () =>{ 
-
-    it('can park', () =>{
-        assert.strictEqual(car.park(), 'stopped');
-    } );
-
-    it('can drive', () =>{
+beforeEach (async () =>{
+  // Get a list of all unlocked eth accounts precreated by ganached 
+  accounts = await web3.eth.getAccounts()
     
-        assert.strictEqual(car.drive(), 'vroom');
-    })
-
+  //Use one of those accounts to deploy contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({data: bytecode, arguments: [HI_THERE]})
+  .send({from: accounts[0], gas: '1000000'})
 });
 
 
 
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+    //Log the inbox contract object
+    // console.log(inbox);
+
+    //Assert that there's an address string in the inbox object 
+    //This tells if the contract was successfully deployed. 
+    assert.ok(inbox.options.address);
+    });
+
+    //Assert that when contract is depoyed, the message property has the default message set
+    it('has a default message', async () =>{
+        const message = await inbox.methods.message().call();
+        assert.ok(message, HI_THERE);
+    });
+
+    it('can change the message', async () => {
+        //Call the setMessage function and specify the account you are using to do the transaction
+       await inbox.methods.setMessage(BYE).send({from : accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.ok(message, BYE);
+
+    });
+
+})
